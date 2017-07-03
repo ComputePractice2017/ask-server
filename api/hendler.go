dfad372a1e63a41fc13a42a9cbcd0f74c9f65bdc
DragonLP11 <blakstar.dragon@yandex.ru>
2017-07-03 18:29:33 +0300

    Добавил функцию для добавления ответа на определенный вопрос isuue #6

--- api/hendler.go
+++ api/hendler.go
@@ -1,11 +1,56 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/ComputePractice2017/ask-server/model"
+	"github.com/gorilla/mux"
 )
 
 func helloMFWorldHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello World MZF!!!")
+}
+
+func newAnswerHandler(w http.ResponseWriter, r *http.Request) {
+
+	var answer string
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+	if err := r.Body.Close(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+	if err := json.Unmarshal(body, &answer); err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+	}
+
+	vars := mux.Vars(r)
+	err = model.NewAnswer(vars["guid1"], vars["id"], answer)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
 }
--- model/fask.go
+++ model/fask.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"strconv"
+
 	r "gopkg.in/gorethink/gorethink.v3"
 )
 
@@ -31,4 +33,35 @@ func InitSession() error {
 		Address: "localhost",
 	})
 	return err
+}
+
+//NewAnswer функция для добовления ответа на определенный вопрос
+func NewAnswer(url string, id string, answer string) error {
+
+	res, err := r.DB("Faskdb").Table("fasker").GetAllByIndex("surl", url).Run(session)
+	if err != nil {
+		return err
+	}
+
+	var f Faskurl
+	err = res.One(&f)
+	if err != nil {
+		return err
+	}
+
+	var nid int
+	nid, err = strconv.Atoi(id)
+	if err != nil {
+		return err
+	}
+	
+	f.Fasks[nid].Answer = answer
+
+	_, err = r.DB("Faskdb").Table("fasker").Get(f.ID).Replace(f).Run(session)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
